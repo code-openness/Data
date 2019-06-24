494b529357196dd14f5333916622dcfb0613ed67
julianewundermann <50406215+julianewundermann@users.noreply.github.com>
2019-06-24 14:43:22 +0200

    function shortenNames (#33)

--- handlers/author_editor.py
+++ handlers/author_editor.py
@@ -1,12 +1,62 @@
 import numpy as np
+import re
+import pandas as pd
 
+# shortens name: Lastname, F.S.
+def shortenNames(column):
+    newValues = []
 
-def RemoveColon(string):
-    return string.replace(':', '')
+    for x, string in column.iteritems():
+        # checkes whether value is nan
+        if (not string) or string != string:
+            newValues.append(np.nan)
+            continue
+        end = []
+
+        names = [name.strip() for name in string.split(';')]
+        for i, name in enumerate(names):
+            ns = name.split(', ')
+            # ns has to have two parts: lastname, firstname
+            if len(ns) == 1:
+                end.append(name)
+                continue
+            firstname = ns[1].strip()
+            lastname = ns[0].replace(',', '').strip()
+
+            # checkes whether the value alreaddy has short parts
+            if (re.search('.\.', firstname)):
+                newFN = firstname.replace('-', '').strip()
+                firstnames = newFN.split(' ')
+
+                fn = ''
+                for piece in firstnames:
+                    if piece == '':
+                        continue
+                    if (re.search('.\.', piece)):
+                        fn += piece
+                    else:
+                        # first letter of name
+                        fn += piece[0] + '.'
+                end.append(lastname + ', ' + fn)
+            else:
+                newFN = firstname.replace('-', ' ')  # .replace(' ', '')
+                firstnames = newFN.split(' ')
+                fn = ''
+                for piece in firstnames:
+                    if piece == '':
+                        continue
+                    if len(piece) == 1:
+                        fn += piece + '.'
+                    else:
+                        # first letter of name
+                        fn += piece[0] + '.'
+                end.append(lastname + ', ' + fn)
+        newValues.append(';'.join(end))
+    return pd.Series(newValues)
 
 
 def author_editor_cleanup(df):
     # some authors names have a : at the end, remove that
-    df['authors'] = df['authors'].astype(str).apply(RemoveColon)
-    df['editors'] = df['editors'].astype(str).apply(RemoveColon)
+    df['authors'] = shortenNames(df['authors'])
+    df['editors'] = shortenNames(df['editors'])
     return df
